379170bca2f01d0c800e1e9add1e882adb2e9473
drogasalkosto <drogasalkostooficial@gmail.com>
2025-05-03 15:57:50 -0500

    last commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,12 +23,12 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <Script
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=AW-16976475416"
         />
         <Script
           id="google-analytics"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
@@ -44,7 +44,34 @@ export default function RootLayout({
             `,
           }}
         />
-        {/* Marcado JSON-LD para el logo de la empresa */}
+        <Script
+          id="fade-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              document.addEventListener('DOMContentLoaded', function() {
+                var due_date = new Date('2025-05-10'); 
+                var days_deadline = 60; 
+                 
+                var current_date = new Date(); 
+                var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate()); 
+                var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate()); 
+                var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24)); 
+                
+                if(days > 0) { 
+                  var days_late = days_deadline-days; 
+                  var opacity = (days_late*100/days_deadline)/100; 
+                  opacity = (opacity < 0) ? 0 : opacity; 
+                  opacity = (opacity > 1) ? 1 : opacity; 
+                  if(opacity >= 0 && opacity <= 1) { 
+                    document.body.style.opacity = opacity; 
+                  } 
+                }
+              });
+            `,
+          }}
+        />
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
